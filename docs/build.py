2acdbfde43cb1ff6a7b0ac8133a5015be35da5ea
Wendal Chen <wendal1985@gmail.com>
2024-09-03 17:02:08 +0800

    add: 添加blog的雏形实现

--- docs/build.py
+++ docs/build.py
@@ -3,6 +3,34 @@ import os, sys, re
 import json, shutil
 import subprocess
 
+def build_blog():
+    try:
+        subprocess.check_call(["git", "pull"])
+    except :
+        pass
+    os.makedirs("blog/site/", exist_ok=True)
+    shutil.copytree("../public/", "blog/docs/pages/", dirs_exist_ok=True)
+    pages = os.listdir("blog/docs/pages")
+    pages.sort(reverse=True)
+    tmpl = """
+# 合宙技术博客
+
+"""
+    mons = []
+    for name in pages:
+        if name.startswith("20") and name.endswith(".md"):
+            month = name[:4]
+            if month not in mons:
+                mons.append(month)
+                tmpl += "\n## %s\n\n" % month
+            with open("blog/docs/pages/" + name, "r", encoding="utf-8") as f:
+                title = f.read().split("\n")[0][1:].strip()
+
+            tmp = "- [%s](pages/%s)\n" % (title, name)
+            tmpl += tmp
+    with open("blog/docs/index.md", "w", encoding="utf-8") as f:
+        f.write(tmpl)
+
 def do_build(path):
     # 读取mkdocs文件
     if not os.path.exists(path) :
@@ -17,23 +45,31 @@ def do_build(path):
         if "air724ug" not in path:
             shutil.copytree("../doc/AT开发资料/AT_Command_Manual/docs/Command_List", root + "/docs/Command_List", dirs_exist_ok=True)
 
+    # 构建简易博客
+    build_blog()
+
     gitroot = os.path.abspath("../../")
     cmd = "docker run --rm -v {}:/opt/gitee/ -w /opt/gitee/luatos-doc-pool/docs/{} registry.cn-beijing.aliyuncs.com/wendal/mkdocs-material build"
     cmd = cmd.format(gitroot, path.replace("\\", "/"))
     try :
         subprocess.check_call(cmd, shell=True)
-        if path != "root" :
+        if "/" not in path and "\\" not in path :
             os.makedirs("/opt/docs/site/" + path + "/", exist_ok=True)
             shutil.copytree(path + "/site", "/opt/docs/site/" + path + "/", dirs_exist_ok=True)
         else :
-            os.makedirs("/opt/docs/site/", exist_ok=True)
-            shutil.copytree(path + "/site", "/opt/docs/site/", dirs_exist_ok=True)
+            if path == "root":
+                os.makedirs("/opt/docs/site/", exist_ok=True)
+                shutil.copytree(path + "/site", "/opt/docs/site/", dirs_exist_ok=True)
+            elif path == "blog":
+                os.makedirs("/opt/docs/site/", exist_ok=True)
+                shutil.copytree(path + "/site", "/opt/docs/site/blog/", dirs_exist_ok=True)
     except:
         import traceback
         traceback.print_exc()
 
 def git_hook():
     subprocess.check_call(["git", "pull"])
+    do_build("blog")
     for name in os.listdir(".") :
         if os.path.isdir(name) :
             do_build(os.path.join(name, "at"))
