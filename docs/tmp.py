364cd5dd38cad8824d9104a17c6c592c1924e338
Wendal Chen <wendal1985@gmail.com>
2024-09-24 08:58:27 +0800

    update: 更新docs的导航目录,以满足20240924的要求

--- docs/tmp.py
+++ docs/tmp.py
@@ -14,12 +14,22 @@ import os, shutil
 #     if os.path.exists(os.path.join(name, "luatos", "docs", "product")) :
 #         shutil.move(os.path.join(name, "luatos", "docs", "product"), os.path.join("root", "docs", name, "product"))
 
-for name in os.listdir("."):
-    if not name.startswith('air'):
-        continue
-    if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart")) :
-        os.makedirs(os.path.join(name, "luatos", "docs", "quickstart"))
-    if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart", "index.md")) :
-        with open(os.path.join(name, "luatos", "docs", "quickstart", "index.md"), "w", encoding="UTF-8") as fp:
-            fp.write("# " + name + " 快速入门\r\n")
-            fp.write("\r\n")
+# for name in os.listdir("."):
+#     if not name.startswith('air'):
+#         continue
+#     if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart")) :
+#         os.makedirs(os.path.join(name, "luatos", "docs", "quickstart"))
+#     if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart", "index.md")) :
+#         with open(os.path.join(name, "luatos", "docs", "quickstart", "index.md"), "w", encoding="UTF-8") as fp:
+#             fp.write("# " + name + " 快速入门\r\n")
+#             fp.write("\r\n")
+
+# for name in os.listdir("."):
+#     if not name.startswith('air'):
+#         continue
+#     if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart")) :
+#         os.makedirs(os.path.join(name, "luatos", "docs", "quickstart"))
+#     if not os.path.exists(os.path.join(name, "luatos", "docs", "quickstart", "index.md")) :
+#         with open(os.path.join(name, "luatos", "docs", "quickstart", "index.md"), "w", encoding="UTF-8") as fp:
+#             fp.write("# " + name + " 快速入门\r\n")
+#             fp.write("\r\n")
