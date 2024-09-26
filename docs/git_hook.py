febb6cba2fb9df3c45c468abeb9cec328fd468d0
Wendal Chen <wendal1985@gmail.com>
2024-09-26 20:10:27 +0800

    update: git_hook钩子最长执行25分钟

--- docs/git_hook.py
+++ docs/git_hook.py
@@ -6,8 +6,8 @@ from bottle import post, run, template
 
 @post('/api/git_hook')
 def index():
-    subprocess.check_call(["git", "pull"])
-    subprocess.check_call(["python3", "build.py"])
+    subprocess.check_call(["git", "pull"], timeout=60*5)
+    subprocess.check_call(["python3", "build.py"], timeout=60*20)
     return "ok"
 
 if __name__ == '__main__':
